e9b3b83318624de374748870c1d830ac7be6b960
Nicholas Smith <nchlssmith1@gmail.com>
2013-01-12 15:14:37 -0600

    create functions to get data from the xml

--- spcc/include/php/config.php
+++ spcc/include/php/config.php
@@ -1,18 +1,18 @@
 <?php
+
 class Config
 {
     private $ini_array;
     private $type;
     private $DOCROOT;
+
     function __construct() {
         //Remeber that the localhost version is in the root not the git folder.
         $this->DOCROOT = $_SERVER['DOCUMENT_ROOT'];
         $this->ini_array = parse_ini_file($this->DOCROOT."/config.ini", true);
         $this->type = $this->ini_array['enviroment']['type'];
-        //print "<pre>";
-        //print_r($this->ini_array);
-        //print "</pre>";
     }
+
     public function getDBSetting($name){
         if ($this->type == "localhost"){
             $data = $this->ini_array['data_base_localhost'];
@@ -21,20 +21,23 @@ class Config
         }
         return (isset($data[$name]))? $data[$name] : "";
     }
+
     public function getLogSetting($name){
         $env = $this->ini_array['enviroment'];
         $data = $this->ini_array['logger'];
-        if($name = "log_path"){
+        if($name == "log_path"){
             return $this->DOCROOT.$env[$this->type."_path"].$data[$name];
         } else {
             return $data[$name];
         }
     }
+
     public function getDataPath(){
         $env = $this->ini_array['enviroment'];
         $data = $this->ini_array['data_path'];
         return $this->DOCROOT.$env[$this->type."_path"].$data;
     }
+
     public function getTmpSetting($name){
         $env = $this->ini_array['enviroment'];
         $data = $this->ini_array['template'];
--- spcc/include/php/inspection.php
+++ spcc/include/php/inspection.php
@@ -5,44 +5,61 @@ include_once "config.php";
 class Inspection {
     private $_xml;
     private $_docRoot;
+    private $_db;
+    private $log;
 
     public function __construct($facility_id){
-        $db = new Database();
+        $this->_db = new Database();
         $config = new Config();
+        $this->log = new KLogger($config->getLogSetting("log_path"),
+                                 $config->getLogSetting("severity"));
+        print $config->getLogSetting("severity");
         $this->_docRoot = $config->getDataPath();
-        $data_path = $db->query("SELECT data_path FROM t_inspection WHERE facility_id = '$facility_id'");
+        $data_path = $this->_db->query("SELECT data_path FROM t_inspection WHERE facility_id = '$facility_id'");
         $this->_xml = simplexml_load_file($this->_docRoot.$data_path[0]['data_path']);
-        die();
+
     }
 
-    public function getEquip($type){
-        if($type == "facility"){
+    /**
+     *  Returns an simplexml object to be passed back for recursiveness.
+     **/
+    public function getEquip($type, $xml=null){
+        if(!$xml) $xml = $this->_xml;
+        if($type == "Facility"){
+            $this->log->logInfo("Returned from Insepction::getEquipID : ", $rtn);
             return $xml;
         } else {
+            $this->log->logInfo("Returned from Insepction::getEquipID : ", $rtn);
             return $xml->xpath("equipment[@type='$type']");
         }
     }
 
+    public function getProperties($name, $xml){
+        $rtn = array();
+        $nm_array = $this->_db->query("SELECT attr.name FROM t_attribute as attr
+                                      INNER JOIN t_equipment as equip
+                                      ON attr.equipment_id = equip.id
+                                      WHERE equip.name = '$name'");
+        foreach($nm_array as $array){
+            $pName = $array['name'];
+            $answer = $xml->xpath("property[@name='$pName']");
+            if(count($answer) > 0){
+                $rtn[$pName] = ''.$answer[0].'';
+            } else {
+                $rtn[$pName] = null;
+            }
+        }
+        $this->log->logInfo("Returned from Insepction::getProperties : ", $rtn);
+    }
+
+    public function debug($array){
+        print "<pre>";
+        print_r($array);
+        print "</pre>";
+
+    }
+
 
 }
-/**
-if($data['type'] == 'updateXML' && $data['value'] != ''){
-  $xml = simplexml_load_file($docRoot.$data['data_path']);
-  $equip = array();
-  if($data['id'] == 0){
-    $equip[0] = $xml;
-  } else {
-    $equip = $xml->xpath("equipment[@id='".$data['id']."']");
-  }
-  $prop = $equip[0]->xpath("property[@name='".$data['name']."']");
-  if(count($prop) == 0){
-    $child = $equip[0]->addChild('property', $data['value']);
-    $child->addAttribute('name', $data['name']);
-  } else {
-    $prop[0]->{0} = $data['value'];
-  }
-  $xml->saveXML($docRoot.$data['data_path']);
-  print json_encode(array('result' => 'true'));
-}
-**/
+
 ?>
