62ba387ee5bd0713581617089c755c873c90a5ca
Nicholas Smith <nsmith@ok7-eleven.com>
2013-01-12 20:51:35 -0600

    build word template create table function

--- spcc/include/php/word_template.php
+++ spcc/include/php/word_template.php
@@ -1,5 +1,31 @@
 <?php
-class SPCCTemplate(){
+include_once "klogger.php";
+include_once "config.php";
+
+class SPCCTemplate{
+    private $log;
+
+    public function __construct(){
+        $config = new Config();
+        $this->log = new KLogger($config->getLogSetting("log_path"),
+                                 $config->getLogSetting("severity"));
+
+    }
+
+    public function createTable($section, $data, $style=null){
+        if(is_array($data)){
+            $table = $section->addTable();
+            foreach($data as $row){
+                $table->addRow();
+                foreach($row as $cellText){
+                    $cell = $table->addCell(2000);
+                    $cell->addText($cellText);
+                }
+            }
+        } else {
+            $this->log->logWarn("Table could not be made, no data was recieved ", $data);
+        }
+    }
 
 }
 
