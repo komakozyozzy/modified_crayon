53808d8ac7a3425f2f01d921b0d79c247585259a
Nicholas Smith <nsmith@ok7-eleven.com>
2013-01-15 21:35:33 -0600

    created a couple of method that get data from the xml and return it in a formated array

--- spcc/include/php/inspection.php
+++ spcc/include/php/inspection.php
@@ -28,15 +28,51 @@ class Inspection {
      *  Returns an simplexml object to be passed back
      *  for recursiveness.
      **/
-    public function getEquip($type, $xml=null){
+    private function _getEquipType($type, $xml=null){
+        $array = array();
         if(!$xml) $xml = $this->_xml;
         if($type == "Facility"){
-            $rtn = $xml;
+            return array(array('parent' => null,
+                               'id' => 0,
+                               'props' => $this->_getProperties($type, $xml)));
         } else {
-            $rtn = $xml->xpath("equipment[@type='$type']");
+            foreach($xml->equipment as $x){
+                foreach($this->_getEquipType($type, $x) as $tmp){
+                    $array[] = $tmp;
+                }
+                //$this->debug($array);
+            }
+            $xpath = $xml->xpath("equipment[@type='$type']");
+            foreach($xpath as $path){
+                $array[] = array('parent' => ''.$xml['id'].'',
+                                          'id' => ''.$path['id'].'',
+                                          'props' => $this->_getProperties($type, $path));
+            }
+            //$this->debug($array);
         }
-        $this->log->logInfo("Returned from Insepction::getEquip : ", $rtn);
-        return $rtn;
+        $this->log->logInfo("Returned from Insepction::getEquip : ", $array);
+        return $array;
+    }
+
+    /**
+     *
+     *  excepts an equipment id
+     *
+     *  Returns an simplexml object
+     **/
+    private function _getEquipId($id, $type, $xml=null){
+        if(!$xml) $xml = $this->_xml;
+        $ary = array();
+        if (''.$xml['id'].'' == $id) {
+            $this->log->logInfo("Returned from Insepction::getEquip : ", $xml);
+            return array('id' => $id,
+                         'props' => $this->_getProperties($type, $xml));
+        } else {
+            foreach($xml as $x){
+                $rtn = $this->_getEquipId($id, $type, $x);
+                if($rtn) return $rtn;
+            }
+        }
     }
 
     /**
@@ -46,7 +82,7 @@ class Inspection {
      * returns an array of property name as the key and the answered question as
      * the value
     **/
-    public function getProperties($name, $xml){
+    private function _getProperties($name, $xml){
         $rtn = array();
         $nm_array = $this->_db->query("SELECT attr.name FROM t_attribute as attr
                                       INNER JOIN t_equipment as equip
@@ -63,6 +99,34 @@ class Inspection {
         }
         $this->log->logInfo("Returned from Insepction::getProperties : ", $rtn);
         return $rtn;
+    }
+
+    /**
+     *
+     * excepts either an id or a type for a piece of equipment
+     * returns an array of properites by id
+     *
+     * array(array(parent => 0, id => 1, props => array(first => first_value, second => sec_value)),
+     *       array(parent => 0, id => 2, props => array(first => first_value, second => sec_value)),)
+     **/
+    public function getEquipment($type, $id=null){
+        if($id){
+            if($type == "Facility"){
+                return array(array('parent' => null,
+                                   'id' => ''.$this->_xml['id'].'',
+                                   'props' => $this->_getProperties("Facility", $this->_xml)));
+            } else {
+                return $this->_getEquipId($id, $type);
+            }
+        } else {
+            if($type == "Facility"){
+                return array(array('parent' => null,
+                                   'id' => ''.$this->_xml['id'].'',
+                                   'props' => $this->_getProperties("Facility", $this->_xml)));
+            } else {
+                return $this->_getEquipType($type);
+            }
+        }
     }
 
     //used to easily display arrays
